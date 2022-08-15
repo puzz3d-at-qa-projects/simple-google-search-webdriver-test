39223b15b4e105eb208be8285f57fdf5f7cd1c67
Dmytro Hryshchenko <puzz3d@gmail.com>
2022-08-15 23:22:16 +0300

    Comments rearranged.

--- src/test/java/GoogleSearchSimpleTest.java
+++ src/test/java/GoogleSearchSimpleTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+
 import java.util.List;
+
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 import static org.testng.Assert.assertTrue;
 
@@ -24,13 +26,21 @@ public class GoogleSearchSimpleTest {
         driver.close();
     }
 
+    /**
+     * This method is used to check that an image tab of search results of the Google site contains images.
+     * We are searching some word.
+     * Following the "Images" link, that has a "data-hveid" attribute, which always ends with a "QAw".
+     * Getting images container.
+     * Gathering "img" tags on this container.
+     * Checking that there are at least 20 images in the list.
+     */
     @Test
     public void testGoogleImageSearchResultsAreImages() {
         driver.get("https://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("octothorpe" + Keys.ENTER);         // searching some word
-        driver.findElement(By.xpath("//a[contains(@data-hveid, 'QAw')]")).click();  // "Images" link has a "data-hveid" attribute, which always ends with a "QAw"
-        WebElement container = driver.findElement(By.xpath("//div[@id='islrg']"));  // getting images container
-        List<WebElement> imgs = container.findElements(By.tagName("img"));                       // gathering "img" tags on this container
-        assertTrue(imgs.size() > 20);                                                   // checking that there are at least 20 images in the list
+        driver.findElement(By.name("q")).sendKeys("octothorpe" + Keys.ENTER);
+        driver.findElement(By.xpath("//a[contains(@data-hveid, 'QAw')]")).click();
+        WebElement container = driver.findElement(By.xpath("//div[@id='islrg']"));
+        List<WebElement> imgs = container.findElements(By.tagName("img"));
+        assertTrue(imgs.size() > 20);
     }
 }
